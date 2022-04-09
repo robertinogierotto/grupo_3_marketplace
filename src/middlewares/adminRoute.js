a81474fdc00d135b25af1fd548fb8bf9fa1e9e84
robertinogierotto <robertino.gierotto@gmail.com>
2022-04-09 16:07:21 -0300

    header changes

--- src/middlewares/adminRoute.js
+++ src/middlewares/adminRoute.js
@@ -2,7 +2,6 @@ module.exports = (req, res, next) => {
     // si existe un admin en session
     if (req.session.user && req.session.user.category == 'admin') {
         // Lo dejamos pasar
-        
         next();
     } else {
         
